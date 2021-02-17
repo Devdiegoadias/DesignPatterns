784cc89401f32f2323e7c4a6e9052b9cc6f1e787
MrTravor <diego.acioly@hotmail.com>
2021-02-17 07:52:03 -0300

    ;

---
+++ Comportamentais/ChainOfResponsability.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Comportamentais.ChainOfResponsability
+{
+    public class TicketAtendimento
+    {
+        public string Conteudo { get; set; }
+        public long Id { get; set; }
+    }
+
+    public class ResultadoDaAnalise
+    {
+        public bool Aprovado { get; set; }
+        public string Revisor { get; set; }
+    }
+
+    public interface IAtendente
+    {
+        ResultadoDaAnalise Resultado(TicketAtendimento ticket);
+    }
+
+    public class Analista : IAtendente
+    {
+        public IAtendente Proximo { get; private set; }
+        public Analista(IAtendente proximo)
+        {
+            Proximo = proximo;
+        }
+
+        public ResultadoDaAnalise Resultado(TicketAtendimento document)
+        {
+            ResultadoDaAnalise resultado = new ResultadoDaAnalise()
+            {
+                Revisor = "TicketAtendimento Analista Nível 1"
+            };
+
+            if (!string.IsNullOrWhiteSpace(document.Conteudo))
+            {
+                if (document.Conteudo.Length > 777)
+                    return Proximo.Resultado(document);
+                if (document.Conteudo.Length <= 777)
+                    resultado.Aprovado = true;
+            }
+            return resultado;
+        }
+    }
+
+    public class EspecialistaTecnico : IAtendente
+    {
+        public ResultadoDaAnalise Resultado(TicketAtendimento document)
+        {
+            ResultadoDaAnalise resultado = new ResultadoDaAnalise()
+            {
+                Revisor = "TicketAtendimento Especialista Técnico Nível 2"
+            };
+            resultado.Aprovado = !string.IsNullOrWhiteSpace(document.Conteudo) && document.Conteudo.Length > 1000;
+            return resultado;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using DesignPatterns.Comportamentais.ChainOfResponsability;
 using DesignPatterns.Comportamentais.Strategy;
 
 
@@ -8,24 +10,48 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            IEstrategiaImposto iTipoPessoaStrategia;
+            /*
+             /////// PADRÃO STRATEGY
+
+             IEstrategiaImposto iTipoPessoaStrategia;
+
+             iTipoPessoaStrategia = new PessoaFisica();
+
+             PagamentoContexto pagamento = new PagamentoContexto(iTipoPessoaStrategia);
+             Console.WriteLine("Imposto: Estratégia para Pessoa Física");
+
+             pagamento.AplicarImposto(10000);
+
+             Console.WriteLine("Imposto: Estratégia para Pessoa Jurídica.");
+
+             iTipoPessoaStrategia = new PessoaJuridica();
+
+             pagamento.SetEstrategiaImposto(iTipoPessoaStrategia);
+
+             pagamento.AplicarImposto(10000);
+
+             Console.ReadLine();
+            */
+
+            /// PADRÃO CHAIN OF RESPONSABILITY
+            /// 
+            List<TicketAtendimento> documents = new List<TicketAtendimento>()
+                {
+                    new TicketAtendimento() { Id = 1, Conteudo = new string('*', 500)},
+                    new TicketAtendimento() { Id = 2, Conteudo = new string('*', 850)},
+                    new TicketAtendimento() { Id = 3, Conteudo = new string('*', 1500) }
+                };
+
+            IAtendente vAnalista = new Analista(new EspecialistaTecnico());
             
-            iTipoPessoaStrategia = new PessoaFisica();
+            documents.ForEach(x =>
+            {
+                var resultado = vAnalista.Resultado(x);
+                Console.WriteLine(resultado.Aprovado ? "Ticket {0} Aceito por {1}"
+                    : "Ticket {0} Não aceito Por {1}",
+                                  x.Id, resultado.Revisor);
+            });
 
-            PagamentoContexto pagamento = new PagamentoContexto(iTipoPessoaStrategia);
-            Console.WriteLine("Imposto: Estratégia para Pessoa Física");
-
-            pagamento.AplicarImposto(10000);
-
-            Console.WriteLine("Imposto: Estratégia para Pessoa Jurídica.");
-
-            iTipoPessoaStrategia = new PessoaJuridica();
-
-            pagamento.SetEstrategiaImposto(iTipoPessoaStrategia);
-            
-            pagamento.AplicarImposto(10000);
-
-            Console.ReadLine();
         }
     }
 }
